472041ae7d6f34f3ea8fb1f02ff1f47e5e22430f
Kate Eldridge <keldridge@somethingdigital.com>
2023-03-27 13:17:35 -0400

    Update api.js

--- src/api.js
+++ src/api.js
@@ -9,4 +9,4 @@ export const geoApiOptions = {
 export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
 
 export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
-export const WEATHER_API_KEY = "94ffe2474bf53a0676fd976d96856ea0";
+export const WEATHER_API_KEY = "Your Key Here";
